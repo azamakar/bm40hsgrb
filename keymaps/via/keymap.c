250226cebb9b2d358f3f8742994e082d1717fb6f
azama <azamakar@gmail.com>
2022-01-26 19:12:40 -0500

    Actualizando capa Qwerty con Tap Dance
    :i

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -19,61 +19,31 @@ enum layers {
   _QWERTY,
   _LOWER,
   _RAISE,
-  _ADJUST,
-  _NUM
+  _ADJUST
 };
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
-#define TABNUM LT(_NUM, KC_TAB)
-#define CAPSFT LT(KC_LSFT, KC_CAPS)
-#define TD0 TD(TD_LSFT_CAPS)
-#define TD1 TD(TD_SPC_ENT)
-
-// Tap Dance declarations
-enum {
-    TD_LSFT_CAPS,
-    TD_SPC_ENT
-};
-
-// Tap Dance definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Left Shift, twice for Caps Lock
-    [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-      // Tap once for Space, twice for Enter
-    [TD_SPC_ENT] = ACTION_TAP_DANCE_DOUBLE(KC_SPC, KC_ENT)
-    // Add tap dance item to your keymap in place of a keycode   
-
-};
-
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * |TabNum|   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | TD0  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Left | Ctrl | Alt  | GUI  |Lower |      TD1    |Raise | RAlt | Down |  Up  |Right |
+ * | BLTog| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
- * 
- * TD0 --> "KC_LSFT" (1 tap and/or hold), otherwise "KC_CAPS" (2 tap rapidly)
- * TD1 --> "KC_SPC" (1 tap and/or hold), otherwise "KC_ENT" (2 tap rapidly)
- * TABNUM --> Tap once for "KC_TAB", hold for _NUM
  */
-
 [_QWERTY] = LAYOUT_planck_mit(
-    TABNUM,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    TD0 , KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LEFT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,  TD1  ,  RAISE,   KC_RALT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
-
-
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
@@ -128,30 +98,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
-),
-
-/* NUM
-     * ,-----------------------------------------------------------------------------------.
-     * | **** |  F1  |  F2  |  F3  |  F4  |  F5  |      |   7  |   8  |   9  |   *  | Del  |
-     * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * |      |  F6  |  F7  |  F8  |  F9  |  F10 | Bksp |   4  |   5  |   6  |   +  |      |
-     * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |      |      |      |      |  F11 |  F12 | Enter|   1  |   2  |   3  |   -  |      |
-     * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |Reset |      |      |      |      |             |   0  |   ,  |   .  |      |      |
-     * `-----------------------------------------------------------------------------------'
-     */
-
-[_NUM] = LAYOUT_planck_mit(
-    _______ , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , XXXXXXX , KC_7 , KC_8  , KC_9    , KC_PAST , KC_DEL  ,
-    XXXXXXX , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_BSPC , KC_4 , KC_5  , KC_6    , KC_PPLS , XXXXXXX ,
-    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_F11  , KC_F12  , KC_ENT  , KC_1 , KC_2  , KC_3    , KC_PMNS , XXXXXXX ,
-    RESET  , XXXXXXX , XXXXXXX , _______ , XXXXXXX ,      XXXXXXX      , KC_0 , KC_COMM ,KC_DOT   , XXXXXXX , XXXXXXX
-    )
+)
 
 };
 
-uint32_t layer_state_set_user(uint32_t state) {
+layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
